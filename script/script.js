ea930887bab43850dc2e426b439fd68bc3e57a92
Lukass <lukassh@gmail.com>
2019-02-14 11:17:49 +0100

    Fix script

--- script/script.js
+++ script/script.js
@@ -2,12 +2,13 @@ var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
   cellAlign: 'left',
+  hash: true,
   contain: true
 });
 
 // element argument can be a selector string
 //   for an individual element
-var flkty = new Flickity( '.main-carousel', {
+/* var flkty = new Flickity( '.main-carousel', {
 	hash: true,
 });
-
+*/
