7ba6c76dbc35d792ed5eaa44e14b976bf3aa3e6c
Lukass <lukassh@gmail.com>
2019-02-14 17:24:17 +0100

    Modify script.js

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 var elem = document.querySelector('.main-carousel');
 var flkty = new Flickity( elem, {
   // options
-  cellAlign: 'left',
+  cellAlign: 'center',
   hash: true,
   contain: true
 });
@@ -12,3 +12,15 @@ var flkty = new Flickity( elem, {
 	hash: true,
 });
 */
+var clickButton = document.querySelector('.restart');
+
+clickButton.addEventListener('click', function() {
+	flkty.select( 0 );
+})
+
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on('scroll', function(progress) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
